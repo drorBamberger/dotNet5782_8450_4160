81b3dea850111652b0eb403778216e05bf421463
sadovski@yatmal.co.il <sadovski@yatmal.co.il>
2021-10-10 23:49:11 +0300

    intial code + welcome functions

---
+++ Targil0/Program4160.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4160();
+            Welcome8450();
+            Console.ReadKey();
+        }
+        static partial void Welcome8450();
+        private static void Welcome4160()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
