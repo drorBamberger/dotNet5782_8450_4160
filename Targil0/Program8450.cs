3d7f1c3e0c024cc99256cf16739b6d1d8e1c28f0
dror bamberger <drorbamberger@gmail.com>
2021-10-11 11:41:56 +0300

    write a func

---
+++ Targil0/Program8450.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome8450()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
