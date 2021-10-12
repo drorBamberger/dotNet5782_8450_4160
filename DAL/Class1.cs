08d9109164c8047fabacea9f8e03aed59196b63e
sadovski@yatmal.co.il <sadovski@yatmal.co.il>
2021-10-12 18:09:18 +0300

    start

--- DAL/Class1.cs
+++ DAL/Class1.cs
@@ -4,14 +4,38 @@ namespace DAL
 {
     namespace DO
     {
-        public class DataSource
+        enum WeightCategories
         {
-            public string Name { get; set; }
+            Light,
+            Medium,
+            Heavy
+        }
+        enum Priorities
+        {
+            Normal,
+            Fast,
+            Emergency
+        }
 
-            public override string ToString()
-            {
-                return Name;
-            }
+        public class Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public double Longitude { get; set; }
+            public double Lattitude { get; set; }
+
+            public override string ToString(){return Name;}
+        }
+        public class Parcel
+        {
+            public int Id { get; set; }
+            public int SenderId { get; set; }
+            public int TargetId { get; set; }
+            public WeightCategories Weight { get; set; }
+            public Priorities Priority { get; set; }
+            public int Droneid { get; set; }
+
         }
     }
 }
