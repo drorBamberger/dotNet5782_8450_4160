e604a2fb07e7d55730c58c76cb5ccdb068fcbf60
dror bamberger <drrobamberger@gmail.com>
2021-10-12 18:18:29 +0300

    final enums

---
+++ DAL/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public enum WeightCategories
+        {
+            Light,
+            Medium,
+            Heavy
+        }
+    }
+
+    public enum Priorities
+    {
+        Normal,
+        Fast,
+        Emergency
+    }
+
+    public enum DroneStatuses
+    {
+        vacant,
+        maintenance,
+        Shipping
+    }
+}
