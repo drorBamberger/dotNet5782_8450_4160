28a8cc4ba57bb9ce17c9bf14eb67c138adcf1c8c
sadovski@yatmal.co.il <sadovski@yatmal.co.il>
2021-10-20 17:41:03 +0300

    some progres

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -8,6 +8,23 @@ namespace DalObject
 
     class DataSource
     {
-        internal Drone[10];
+        internal Drone[] drones = new Drone[10];
+        internal Station[] stations = new Station[5];
+        internal Customer[] customers = new Customer[100];
+        internal Parcel[] parcels = new Parcel[1000];
+
+        internal class config
+        {
+            //folowing the first - not full place in every array
+            internal static int freeDrone = 0;
+            internal static int freeStation = 0;
+            internal static int freeCustomer = 0;
+            internal static int freeParcel = 0;
+
+
         }
+
+
+    }
+
 }
