46075341f639530c9de1788bd6be7f37e86385f9
sadovski@yatmal.co.il <sadovski@yatmal.co.il>
2021-11-09 14:37:58 +0200

    +BL

---
+++ BL/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBL
+{
+    namespace BO
+    {
+        public enum WeightCategories
+        {
+            Light,
+            Medium,
+            Heavy
+        }
+
+
+        public enum Priorities
+        {
+            Normal,
+            Fast,
+            Emergency
+        }
+
+    }
+}
