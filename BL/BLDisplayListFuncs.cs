7305276fe68620401cc049d78ab7476df1db1ad9
sadovski@yatmal.co.il <sadovski@yatmal.co.il>
2021-11-15 22:13:55 +0200

    +stationList

--- BL/BLDisplayListFuncs.cs
+++ BL/BLDisplayListFuncs.cs
@@ -7,6 +7,24 @@ namespace BL
 {
     public partial class BL : IBL.IBL
     {
-
+        public IEnumerable<StationForList> StationList()
+        {
+            IEnumerable<IDAL.DO.Station> original = MyDal.StationList();
+            List<StationForList> comeBack = new List<StationForList>();
+            int counter = 0;
+            
+            foreach(var item in original)
+            {
+                foreach (var drone in Drones)
+                {
+                    if (drone.MyLocation == new Location(item.Longitude, item.Lattitude))
+                        counter++;
+                        
+                }
+                comeBack.Add(new StationForList(item.Id, item.Name, item.ChargeSlots, counter));
+                counter = 0;
+            }
+            return comeBack;
+        }
     }
 }
--- BL/StationForList.cs
+++ BL/StationForList.cs
@@ -8,6 +8,14 @@ namespace IBL
     {
         public class StationForList
         {
+            public StationForList(int id, string name, int freeChargeSlots, int occupiedChargeSlots)
+            {
+                Id = id;
+                Name = name;
+                FreeChargeSlots = freeChargeSlots;
+                OccupiedChargeSlots = occupiedChargeSlots;
+            }
+
             public int Id { get; set; }
             public string Name { get; set; }
             public int FreeChargeSlots { get; set; }
